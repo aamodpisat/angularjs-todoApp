45605077c8b543eb3f2a9d84c396745ebc7ab8be
Aamod Pisat <aamod.pisat@raweng.com>
2016-02-16 14:33:40 +0530

    Display todolist and status

--- js/demo.js
+++ js/demo.js
@@ -1,12 +1,14 @@
 
-	var date= new Date().toISOString().substring(0, 10);
+	var date= new Date().toISOString().substring(0, 10),
+		task= [];
   		
   		// add function...
   	function add (arg) {
   		var title= document.getElementById('title').value,
 			description= document.getElementById('description').value,
 			start_date= document.getElementById('start_date').value,
-  			end_date= document.getElementById('end_date').value;
+  			end_date= document.getElementById('end_date').value,
+  			lists= {};
   		
   		
   			var formValidate = validate();
@@ -27,7 +29,7 @@
 			  				alert("Please specify START date");
 			  				flag = false;
 
-			  			} else if(date > start_date ){
+			  			} else if(date > start_date){
 				  					alert("Please enter valid date");
 				  					flag = false;
 
@@ -36,21 +38,65 @@
 			  				flag = false;
 
 			  			} else if (date > end_date ){
-			  				alert("Sorry, the end date you specify is already gone... Thats mean your task is completed...")
+			  				alert("Sorry, the END date you specify is already gone... Thats mean your task is completed...")
 			  				flag = false;
 
 			  			}
 			  			else{
 			  				flag = true;
 			  			}
-			  			console.log(flag);
+			  			
 			  			return flag;
 			  	}
   			
   			if(formValidate){
+  				var status;
+  				if(start_date==date){
+  					status = 'ACTIVE';
+  				}
+  				if(end_date==date){
+  					status = 'WARNING';
+  				}
+  				if(start_date > date || end_date < date){
+  					status = 'PROGRESS';
+  				}
+  				
+  				lists= {
+  							'TITLE': title, 
+  							'DESCRIPTION': description , 
+  							'START_DATE': start_date , 
+  							'END_DATE': end_date,
+  							'STATUS' : status
+  					};
+  				task.push(lists);
+  				var html= '<table cellspacing= "10" padding="5">' +
+  						  '<tr>'+
+  						  '<td>Title</td>'+
+  						  '<td>Description</td>'+
+  						  '<td>Start Date</td>'+
+  						  '<td>End Date</td>'+
+  						  '<td>Status</td>'+
+  						  '</tr>'	
+	  			for(var i =0; i<task.length; i++){
+	  				    html+="<tr>";
+	        			html+="<td>"+task[i].TITLE+"</td>";
+	        			html+="<td>"+task[i].DESCRIPTION+"</td>";
+	       				html+="<td>"+task[i].START_DATE+"</td>";
+	       				html+="<td>"+task[i].END_DATE+"</td>";
+	       				html+="<td>"+task[i].STATUS+"</td>";
+	        			html+="</tr>";
+	  				
+	  				}
+	  			html+="</table>";
+	  			document.getElementById('box').innerHTML= html;
   				alert("Okay you have successfully added task.. Now Go and process the task");
+  			
   			}
+
+ 		return false;
   	}
+
+
 
 
   
